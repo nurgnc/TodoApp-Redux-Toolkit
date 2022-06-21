babf51bbfd49a109ae91beb106155fb8bbf7e616
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2022-06-21 20:26:08 +0300

    created redux structure

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import TodoList from "./TodoList";
+import ContentFooter from "./ContentFooter";
 
 const Content = () => {
   return (
-    <section className="main">
-      <input type="checkbox" className="toggle-all" />
-      <label htmlFor="toggle-all">Mark all as complete</label>
-      <TodoList />
-    </section>
+    <>
+      <section className="main">
+        <input type="checkbox" className="toggle-all" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <TodoList />
+      </section>
+      <ContentFooter />
+    </>
   );
 };
 
