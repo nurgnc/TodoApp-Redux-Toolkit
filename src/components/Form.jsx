d5cc5791734338fd2743a93b9002a572ecd300fd
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2022-06-22 11:01:18 +0300

    added addTodo, toggle and destroy actions

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todos/todoSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Form = () => {
+  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo({ id: nanoid(), title, comleted: false }));
+    setTitle("");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="new-todo"
-        autoFocus
         placeholder="What needs to be done?"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        autoFocus
       />
     </form>
   );
--- src/redux/todos/todoSlice.js
+++ src/redux/todos/todoSlice.js
@@ -7,14 +7,31 @@ export const todoSlice = createSlice({
       {
         id: "1",
         title: "Learn React",
+        completed: true,
       },
       {
         id: "2",
         title: "Read a book",
+        completed: false,
       },
     ],
   },
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload);
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed;
+    },
+    destroy: (state, action) => {
+      const id = action.payload;
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
+    },
+  },
 });
 
+export const { addTodo, toggle, destroy } = todoSlice.actions;
 export default todoSlice.reducer;
